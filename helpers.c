647bf7eeaed533dca1e3c2daad62aa395cbab7ce
faran <faran.allah.verdi1386@gmail.com>
2025-03-19 19:33:33 +0330

    added sepia fiter

--- helpers.c
+++ helpers.c
@@ -12,10 +12,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             // calculate the average of the rgb in each pixel
 
             int color = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
-
-            image[i][j].rgbtBlue = color;
-            image[i][j].rgbtGreen = color;
+            
             image[i][j].rgbtRed = color;
+            image[i][j].rgbtGreen = color;
+            image[i][j].rgbtBlue = color;
         }   
     }
 }
@@ -23,7 +23,27 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            /*
+            sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
+
+            sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
+
+            sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
+            */
+
+            int originalRed = image[i][j].rgbtRed;
+            int originalGreen = image[i][j].rgbtGreen;
+            int originalBlue = image[i][j].rgbtBlue;
+
+            image[i][j].rgbtRed = round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
+            image[i][j].rgbtGreen = round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
+            image[i][j].rgbtBlue = round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
+        }  
+    }
 }
 
 // Reflect image horizontally
