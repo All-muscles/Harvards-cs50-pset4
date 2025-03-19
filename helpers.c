fffd2ca9ad910b94ec2ac6de8d8aceebeb7c8b6e
faran <faran.allah.verdi1386@gmail.com>
2025-03-19 19:20:39 +0330

    added the grayscale filter

--- helpers.c
+++ helpers.c
@@ -1,9 +1,23 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    // loop over each pixel
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // calculate the average of the rgb in each pixel
+
+            int color = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
+
+            image[i][j].rgbtBlue = color;
+            image[i][j].rgbtGreen = color;
+            image[i][j].rgbtRed = color;
+        }   
+    }
 }
 
 // Convert image to sepia
