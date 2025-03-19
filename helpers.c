069fb223927597d643ed1326b27e3f649d268a8e
faran <faran.allah.verdi1386@gmail.com>
2025-03-20 01:03:19 +0330

    added the blur function

--- helpers.c
+++ helpers.c
@@ -94,5 +94,49 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    // an array to copy each pixel that has been blurred to
+    RGBTRIPLE copy[height][width];
+
+    // go through each pixel
+    for (int pixel_row = 0; pixel_row < height; pixel_row++)
+    {
+        for (int pixel_coloumn = 0; pixel_coloumn < width; pixel_coloumn++)
+        {
+            // the main image is our reference and we will write the blurred image onto the copy array, then at the end will copy the copy into the original image array
+            int totalRed = 0, totalGreen = 0, totalBlue = 0, count = 0;
+
+            for (int row_off_set = -1; row_off_set <= 1; row_off_set++)
+            {
+                for (int column_off_set = -1; column_off_set <= 1; column_off_set++)
+                {
+                    int neighbor_row = row_off_set + pixel_row;
+                    int neighbor_column = column_off_set + pixel_coloumn;
+
+                    // checks if tht neighbor_row and neighbor_column are not out of bound
+                    if (neighbor_row >= 0 && neighbor_row < height && neighbor_column >= 0 && neighbor_column < width)
+                    {
+                        totalRed += image[neighbor_row][neighbor_column].rgbtRed;
+                        totalGreen += image[neighbor_row][neighbor_column].rgbtGreen;
+                        totalBlue += image[neighbor_row][neighbor_column].rgbtBlue;
+                        count++;
+                    } 
+                }
+            }
+
+            copy[pixel_row][pixel_coloumn].rgbtRed = round((float) totalRed / count);
+            copy[pixel_row][pixel_coloumn].rgbtGreen = round((float) totalGreen / count);
+            copy[pixel_row][pixel_coloumn].rgbtBlue = round((float) totalBlue / count);
+        }
+    }
+
+    // copy the copy array into the image array
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtRed = copy[i][j].rgbtRed;
+            image[i][j].rgbtGreen = copy[i][j].rgbtGreen;
+            image[i][j].rgbtBlue = copy[i][j].rgbtBlue;
+        }
+    }
 }
